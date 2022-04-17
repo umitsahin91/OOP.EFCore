5b436475f006bd11c6b79236a5ec526d143bc7f4
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-17 07:22:24 +0300

    Entity property constraint

---
+++ OOP.EFCore.ConsoleApp/Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.EFCore.ConsoleApp.Entities
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string Title { get; set; }
+    }
+}
